a1875accc0dbddefdc443b89834bcd8847c95c8d
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-04-26 15:11:46 +0300

    update user info now works

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import validator from 'mongoose-unique-validator';
 import bcrypt from 'bcrypt';
-// import passport from 'passport';
-// import flash from 'express-flash';
-// import session from 'express-session';
-// import localStrategy from 'passport-local';
-// import passportJWT from 'passport-jwt';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -20,31 +15,11 @@ let options = {
 };
 
 let geoCoder = nodeGeocoder(options);
-
-// import initializePassport from './passport-config.js';
-// initializePassport(
-//     passport,
-//     email => users.find(user => user.email === email));
-
-// import apiRouter from './routes/api.js';
-
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use(flash());
-// app.use(session({
-//     session: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }))
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use('/api', apiRouter)
-
 const CONNECTION_URL = 'mongodb+srv://staffan5:test123@gettingstarted.hgzw7.mongodb.net/bandmatefinder?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
@@ -214,14 +189,12 @@ app.post('/register', async(req, res) => {
                 .catch(err => {
                     return res.status(500).send(`An error occured while registering a new user`);
                 })
-
-            // res.end();
         })
     })
 })
 
 //new login code
-app.use('/login', (req, res) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
     User.findOne({ username }, (err, user) => {
@@ -247,30 +220,6 @@ app.use('/login', (req, res) => {
     })
 });
 
-//old code
-// app.post('/login', (req, res) => {
-//     const { username, password } = req.body;
-
-//     User.findOne({ username }, (err, user) => {
-//         if (err) {
-//             console.log('an error has occured')
-//             res.status(500).send()
-//             return;
-//         }
-
-//         if (!user) {
-//             res.status(403).send();
-//             return;
-//         }
-
-//         bcrypt.compare(password, user.password, (err, result) => {
-//             if (err) res.status(500).send();
-//             if (!result) res.status(403).send();
-//             else res.status(200).send();
-//         })
-//     })
-// })
-
 app.get('/getall', (req, res)=>{
     User.find({}, (err, users)=>{
         if(err) console.log(err);
@@ -279,11 +228,23 @@ app.get('/getall', (req, res)=>{
 })
 
 app.get('/getuser/:username', (req, res) => {
-    const username = req.params.username;
+    const { username } = req.params;
     User.findOne({username}, (err, user) => {
         if(err) console.log(err);
         res.json({user});
     })
+})
+
+app.patch('/user/:id', async(req, res) => {
+    const { id } = req.params;
+    const { username, password, city, postalCode, primaryInstrument, freeText, skillLevel, lookingFor } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ${_id}`);
+
+    const updatedUser = {id, username, password, city, postalCode, primaryInstrument, freeText, skillLevel, lookingFor, _id: id}
+    await User.findByIdAndUpdate(id, updatedUser, { new: true })
+
+    res.json({updatedUser, message: 'update ok'});
 })
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //returns promise
